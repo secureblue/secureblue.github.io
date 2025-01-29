763c8b7e18636ad11dbba0d1c31d7e9d63c6c3f4
Bruno <103858681+EsseLowNitro@users.noreply.github.com>
2025-01-29 17:33:14 -0300

    yup

--- _plugins/plugin.rb
+++ _plugins/plugin.rb
@@ -1,5 +1,5 @@
 module Jekyll
-    class HeadingLinkwrap < Converter
+    class secureblue.dev < Converter
         priority :normal
 
         def matches(ext)
@@ -11,19 +11,30 @@ module Jekyll
         end
 
         def convert(content)
+            # Seeks tables of contents and wraps them in <nav> elements
             content = content.gsub(%r{(#+)\s+(.+)\s*\{:\s+#table-of-contents\}}) do |match|
                 level = $1.length
                 text = $2
-                id = "table-of-contents-is-this-working"
+                id = "table-of-contents"
                 "<nav><h#{level} id=\"#{id}\"><a href=\"##{id}\">#{text}</a></h#{level}></nav>"
             end
-            
+
+            # Seeks every heading with a custom ID and wraps them in a self-referential anchor link
             content = content.gsub(%r{(#+)\s+(.+)\s*\{:\s+#([^\}]+)\}}) do |match|
                 level = $1.length
                 text = $2
                 id = $3
                 "<h#{level} id=\"#{id}\"><a href=\"##{id}\">#{text}</a></h#{level}>"
             end
+
+            # Seeks every heading without a custom ID and wraps them in a self-referential anchor link
+            content = content.gsub(%r{^(#+)\s+(.+)\s$^((?!\s*\{:\s+#))$}) do |match|
+                level = $1.length
+                text = $2
+                id = $2.downcase.gsub(/\s+/, "-")
+                "<h#{level} id=\"#{id}\"><a href=\"##{id}\">#{text}</a></h#{level}>"
+            end
+        end
         end
     end
 end
