afadb77cb91e06dacffc84bfee1648d225639c40
Bruno <103858681+EsseLowNitro@users.noreply.github.com>
2025-02-04 12:21:14 -0300

    fix: lack of recommended ruby code practices

--- _plugins/plugin.rb
+++ _plugins/plugin.rb
@@ -3,32 +3,39 @@ module Jekyll
     class Secureblue < Converter
         priority :normal
 
-        def matches(ext)
-            ext =~ /^\.md$/i
-        end
-
-        def output_ext(ext)
-            ".html"
-        end
-
-        def convert(content)
-            # Seeks every heading with a custom ID and wraps them in a self-referential anchor link
-            content = content.gsub(%r{(#+)\s+(.+)\s*\{:\s+#([^\}]+)\}}) do |match|
+        # Seeks every heading with a custom ID and wraps them in a self-referential anchor link
+        def convert_custom_headings(content)
+            content.gsub(%r{(#+)\s+(.+)\s*\{:\s+#([^\}]+)\}}) do
                 level = $1.length
                 text = $2
                 id = $3
 
                 "<h#{level} id=\"#{id}\"><a href=\"##{id}\">#{text}</a></h#{level}>"
             end
+        end
 
-            # Seeks every heading without a custom ID and wraps them in a self-referential anchor link
-            content = content.gsub(%r{(#+)\s+(.+)(.*(?!\n\r?\{.*))}) do |match|
+        # Seeks every heading without a custom ID and wraps them in a self-referential anchor link
+        def convert_regular_headings(content)
+            content.gsub(%r{(#+)\s+(.+)(.*(?!\n\r?\{.*))}) do
                 level = $1.length
                 text = $2
                 id = $2.downcase.gsub(/\s+/, "-")
 
                 "<h#{level} id=\"#{id}\"><a href=\"##{id}\">#{text}</a></h#{level}>"
             end
+        end
+
+        def matches(ext)
+            ext =~ /^\.md$/i
+        end
+
+        def output_ext(_ext)
+            ".html"
+        end
+
+        def convert(content)
+            content = convert_custom_headings(content)
+            convert_regular_headings(content)
         end
     end
 end
